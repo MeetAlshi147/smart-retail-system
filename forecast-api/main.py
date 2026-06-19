76f6237250388775312efaa103f471b410c2756a
Meet Alshi <meetalshi19@gmail.com>
2026-06-19 23:33:36 +0530

    Add CORS support to forecast API endpoint

--- forecast-api/main.py
+++ forecast-api/main.py
@@ -1,12 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from google.cloud import bigquery
-#trial
+
 app = Flask(__name__)
 
 client = bigquery.Client()
 
-@app.route("/forecast")
+@app.route("/forecast", methods=["GET", "OPTIONS"])
 def forecast():
+
+    # CORS Preflight Request
+    if request.method == "OPTIONS":
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type"
+        }
+        return ("", 204, headers)
 
     query = """
     SELECT
@@ -30,11 +39,18 @@ def forecast():
         })
         total += row.forecast_value
 
-    return jsonify({
+    response = jsonify({
         "tomorrowRevenue": forecasts[0]["revenue"],
         "nextWeekRevenue": round(total, 2),
         "forecast": forecasts
     })
+
+    # CORS Headers
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
