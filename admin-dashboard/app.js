5896483ef7eab0cea6d4eabccb9ddf13ee2e533f
Meet Alshi <meetalshi19@gmail.com>
2026-06-20 12:14:48 +0530

    Add forecast and analytics data loading functions

--- admin-dashboard/app.js
+++ admin-dashboard/app.js
@@ -1,1 +1,49 @@
-console.log("APP LOADED");
+const formatINR = (value) => {
+    return `₹ ${(value / 100000).toFixed(2)} Lakh`;
+};
+
+// Forecast Data
+async function loadForecast() {
+    try {
+        const response = await fetch(
+            "https://forecast-api-195476262139.asia-south1.run.app/forecast"
+        );
+
+        const data = await response.json();
+
+        document.getElementById("tomorrow").innerHTML =
+            formatINR(data.tomorrowRevenue);
+
+        document.getElementById("nextweek").innerHTML =
+            formatINR(data.nextWeekRevenue);
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Analytics Data
+async function loadAnalytics() {
+    try {
+        const response = await fetch(
+            "https://forecast-api-195476262139.asia-south1.run.app/analytics"
+        );
+
+        const data = await response.json();
+
+        document.getElementById("orders").innerHTML =
+            data.totalOrders.toLocaleString();
+
+        document.getElementById("revenue").innerHTML =
+            "₹ " + Number(data.totalRevenue).toLocaleString("en-IN");
+
+        document.getElementById("product").innerHTML =
+            data.topProduct;
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+loadForecast();
+loadAnalytics();
